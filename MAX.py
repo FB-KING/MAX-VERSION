39514f961f888887289fc4c1bc213891702a511d
Mahin Ahmed <106468659+FB-KING@users.noreply.github.com>
2022-10-17 19:13:32 +0600

    Add files via upload

---
+++ MAX.py
@@ -0,0 +1,33 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+
+        os.system('xdg-open https://www.facebook.com/FB.KING.MAHIN.NAME.TOH.SONSO')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from max import check_my_key
+ 
+        check_my_key()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from max import check_my_key
+ 
+ 
+        check_my_key()
+        
